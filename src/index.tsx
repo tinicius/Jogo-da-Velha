21c81399ddab2ee1f35f136ca5d145ffdbb2ee59
Vinicius Alves Pereira <vinicius.apereira04@gmail.com>
2023-07-19 11:44:13 -0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { App } from './pages/App';
+
+import { Home } from "./pages/Home"
+import { App } from "./pages/App"
 
 const router = createBrowserRouter([
   {

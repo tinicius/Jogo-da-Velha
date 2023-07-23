f2c4e1e9a3b3435cef73ce137e034b1adfba1933
annalaurams <nalauramoura@gmail.com>
2023-07-23 14:09:05 -0300

    a

--- src/pages/Config/script.js
+++ src/pages/Config/script.js
@@ -10,11 +10,43 @@ const increase = () => {
     document.getElementById('numPartidas').innerHTML++;
 }
 
+const start = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+
+    let player1 = document.getElementById('player1').value;
+    let player2 = document.getElementById('player2').value;
+
+    const modality = urlParams.get('modality');
+
+    if (modality === '1xcpu') {
+        if (!player1) alert("Digitar jogador 1");
+        else {
+            localStorage.setItem('player1', player1);
+            window.location.replace('../Play/index.html');
+        }
+    } else {
+        if (!player1) alert("Digitar jogador 1");
+        else if (!player2) alert("Digitar jogador 2");
+        else {
+            localStorage.setItem('player1', player1)
+            localStorage.setItem('player2', player2)
+            window.location.replace('../Play/index.html');
+        }
+    }
+
+
+
+
+    console.log(player1);
+
+}
+
 window.onload = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const modality = urlParams.get('modality');
 
-    const player2_container = document.getElementById('player2_container');
+    const player2_container = document.getElementById('player2');
 
     if (modality === '1xcpu')
         player2_container.style.display = 'none';
---
+++ src/pages/Play/script.js
@@ -0,0 +1,102 @@
+let isPlayerOne = true;
+let canPlay = true;
+
+const check = (a, b, c) => {
+
+    if (!a || !b || !c) return false;
+
+    if (a == b && b == c) return true;
+    else return false;
+}
+
+const win = () => {
+    const c1 = document.getElementById(`ceil-1`).innerText;
+    const c2 = document.getElementById(`ceil-2`).innerText;
+    const c3 = document.getElementById(`ceil-3`).innerText;
+    const c4 = document.getElementById(`ceil-4`).innerText;
+    const c5 = document.getElementById(`ceil-5`).innerText;
+    const c6 = document.getElementById(`ceil-6`).innerText;
+    const c7 = document.getElementById(`ceil-7`).innerText;
+    const c8 = document.getElementById(`ceil-8`).innerText;
+    const c9 = document.getElementById(`ceil-9`).innerText;
+
+    return check(c1, c2, c3) || check(c4, c5, c6) || check(c7, c8, c9) ||
+        check(c1, c4, c7) || check(c2, c5, c8) || check(c3, c6, c9) ||
+        check(c1, c5, c9) || check(c3, c5, c7)
+
+}
+
+const clean = () => {
+    document.getElementById(`ceil-1`).innerText = "";
+    document.getElementById(`ceil-2`).innerText = "";
+    document.getElementById(`ceil-3`).innerText = "";
+    document.getElementById(`ceil-4`).innerText = "";
+    document.getElementById(`ceil-5`).innerText = "";
+    document.getElementById(`ceil-6`).innerText = "";
+    document.getElementById(`ceil-7`).innerText = "";
+    document.getElementById(`ceil-8`).innerText = "";
+    document.getElementById(`ceil-9`).innerText = "";
+}
+
+const clickCeil = (e) => {
+
+    let content = document.getElementById(`ceil-${e}`);
+
+    if (content.innerHTML) return;
+    if (!canPlay) return;
+
+    if (isPlayerOne)
+        content.innerHTML = 'X';
+    else
+        content.innerHTML = 'O';
+
+    if (win()) {
+
+        canPlay = false;
+
+        setTimeout(() => {
+            let round = localStorage.getItem('round');
+            localStorage.setItem('round', Number(round) + 1);
+            refreshRound();
+            clean();
+            changePoints();
+            canPlay = true;
+            isPlayerOne = true;
+            isPlayerOne = !isPlayerOne;
+        }, 1000);
+
+    } else {
+        isPlayerOne = !isPlayerOne;
+    }
+
+
+}
+
+const refreshRound = () => {
+    if (!localStorage.getItem('round')) {
+        localStorage.setItem('round', 0)
+        document.getElementById('roundText').innerHTML = "Rodada 0";
+    }
+    else
+        document.getElementById('roundText').innerHTML = "Rodada " + localStorage.getItem('round');
+}
+
+window.onload = () => {
+
+    localStorage.setItem('round', 0)
+
+    document.getElementById('player1').innerHTML = localStorage.getItem('player1');
+    document.getElementById('player2').innerHTML = localStorage.getItem('player2');
+
+    refreshRound();
+}
+
+const changePoints = () => {
+
+    if (isPlayerOne) {
+        document.getElementById('points1').innerHTML++;
+    }
+    else {
+        document.getElementById('points2').innerHTML++;
+    }
+}
